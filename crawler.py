89334656af16eb421ecd8233096adae77bbaaef5
Krysten Thomas <krysten@knights.ucf.edu>
2012-01-24 02:56:17 -0500

    program now properly exits when max_depth is reached

--- crawler.py
+++ crawler.py
@@ -32,7 +32,7 @@ def unvisited_test(n):
 
 url_seed = raw_input("Please enter a url seed: ");
 string = raw_input("Please enter search string: ");
-max_depth = raw_input("Please enter the max depth to crawl to: ");
+max_depth = int(raw_input("Please enter the max depth to crawl to: "));
 	
 #create visited and unvisited lists as well as a temp list to hold unvisited links during iteration
 unvisited = deque([])
@@ -44,7 +44,9 @@ unvisited.append(url_seed)
 
 currenturl = unvisited.popleft()
 
-while(unvisited != [] or depth <= max_depth):
+while(unvisited != []):
+	if depth > max_depth: break
+	
 	try:
 		#open the url for parsing
 		url = urllib2.urlopen(currenturl) 
@@ -54,23 +56,24 @@ while(unvisited != [] or depth <= max_depth):
 		
 		#this will see if string is on current html page
 		#regexp is looking for an occurrence of string not the exact match to the NavigableString string
-		find_string = soup.body.find(text=re.compile(string))
-		#find_string = soup.body.findAll(text=re.compile(string), limit=1) 
+		#find_string = soup.body.find(text=re.compile(string))
+		find_string = soup.body.findAll(text=re.compile(string), limit=1) 
 		
 		#if the keyword is found, print that the string is not on the currenturl
-		if find_string == []: print string, "was not found on the page: ", currenturl 
+		if find_string == []: 
+			print string, "was not found on the page: ", currenturl 
 		#otherwise, print that the keyword was not foudn on the currenturl
-		else: print string, "was found on the page: ", currenturl
+		else: 
+			print string, "was found on the page: ", currenturl
 		
 		#Find all links in anchor tags and extract only the links
 		links =  soup.findAll('a', href = re.compile("http://")) 
-		print len(links)
 						
 		#this should only happen once, since unvisited list won't be populated with links in the first iteration
 		if(unvisited == deque([]) and visited == deque([])):
 			for link in links:
-				if validate(link['href']) == 1:
-					unvisited.append(link['href'])
+				if validate(link['href']) == 1 and visited_test(link['href']) == 1 and unvisited_test(link['href']) == 1 and len(temp) < 10:
+					temp.append(link['href'])
 					
 		#Loop through all links and check to see if link is valid and not in visited or unvisited list -> if so then add link to a temporary list [threshold is 50]
 		else:
@@ -79,15 +82,15 @@ while(unvisited != [] or depth <= max_depth):
 					temp.append(link['href'])
 					
 									
-			#extend unvisited list with all of the links accumulated in current iteration
-			unvisited.extend(temp)
+		#extend unvisited list with all of the links accumulated in current iteration
+		unvisited.extend(temp)
 			
-			#free memory in temp list
-			del temp[:]
+		#free memory in temp list
+		del temp[:]
 			
-			#each time temp list is filled with 10 new links, a new depth has been reached
-			depth = depth + 1
-			print "current depth is ", depth
+		#each time temp list is filled with 10 new links, a new depth has been reached
+		depth = depth + 1
+		print "current depth is ", depth
 			
 		#move current url to visited list
 		visited.append(currenturl)
