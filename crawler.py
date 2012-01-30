ac78394a4c03b02bb5185372c93c6c8097e04e09
Krysten Thomas <krysten@knights.ucf.edu>
2012-01-30 00:09:54 -0500

    minor changes to stable version

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ from httplib import BadStatusLine
 
 depth = 0
 
-#this function will check to see if a link is valid
+#this function will see if a link is valid
 def validate(n):
     try:
         currenturl = urllib2.urlopen(n) 
@@ -27,7 +27,7 @@ def unvisited_test(n):
         return 1
     return 0
 	
-#keep asking user for link until it is valid
+#user has to enter a valid url
 while 1:
     try:
         url_seed = raw_input("Please enter a url seed: ");
@@ -37,7 +37,14 @@ while 1:
         print "Oops! Please enter a valid link"
 
 string = raw_input("Please enter search string: ");
-max_depth = int(raw_input("Please enter the max depth to crawl to: "));
+
+#user has to enter an integer 
+while 1:
+    try:
+        max_depth = int(raw_input("Please enter the max depth to crawl to: "));
+        break
+    except (ValueError):
+        print "Ooops! Please enter an integer"
 
 print "\n"
 	
@@ -51,7 +58,7 @@ unvisited.append(url_seed)
 
 currenturl = unvisited.pop(0)
 
-#keep looping until user's max_depth is reached
+#keep looping until user's max_depth is reached or there are no more links to parse
 while(depth < max_depth):	
     try:
         #move current url to visited list
@@ -61,20 +68,26 @@ while(depth < max_depth):
 	url = urllib2.urlopen(currenturl) 
 		
 	#create BeautifulSoup object
-	soup = BeautifulSoup(url) 
+	soup = BeautifulSoup(url)
 		
-	#this will see if string is on current html page
-	#regexp is looking for an occurrence of string not the exact match to the NavigableString string
-	find_string = soup.body.findAll(text=re.compile(string), limit=1) 
+        if soup != None:
+            #this will see if the keyword is on the current html page
+            #regexp looks for an occurence of the keyword
+            find_string = soup.findAll(text=re.compile(string), limit=1)
+        else:
+            print "could not search", currenturl, "for", string
 		
-	#if the keyword is found, print that the string was found on the currenturl
+	#if the keyword is not found, print that the keyword was not found on the currenturl
 	if find_string == []: 
             print string, "was not found on the page: ", currenturl 
-	#otherwise, print that the keyword was not found on the currenturl
+	#otherwise, print that the keyword was found on the currenturl
 	else: 
             print string, "was found on the page: ", currenturl
+
+        #reset list
+        find_string = []
 		
-	#Find all links in anchor tags and extract only the links
+	#Find all html links in anchor tags and extract only the url
 	if soup != None:
             links =  soup.findAll('a', href = re.compile("http://")) 
 			
@@ -85,43 +98,49 @@ while(depth < max_depth):
                     if temp.count(link['href']) == 0:
                         temp.append(link['href'])
 					
-	#Loop through all links and check to see if link is valid and not in visited or unvisited list -> if so then add link to a temporary list [threshold is 10]
+	#Loop through all links and check to see if link is valid and not in visited or unvisited list -> if so then add link to a temporary list [max is 10]
 	else:
             for link in links:
                 if validate(link['href']) == 1 and visited_test(link['href']) == 1 and unvisited_test(link['href']) == 1 and len(temp) < 10:
                     if temp.count(link['href']) == 0:
                         temp.append(link['href'])
-					
 									
-	#Add recently fetched links to the end of unvisited
+	#Add recently fetched links to the end of unvisited list
 	unvisited.extend(temp)
 		
-	#if no links were received during current iteration, fetch link from previous iteration
+	#if no links were received during current iteration, fetch link from previous iteration; if there are no links in unvisited list break out of loop
 	if len(temp) == 0:
-            firstlink = unvisited.pop()
-	    print "no valid links were found on", currenturl, "retrieving new link..."
-	    currenturl = firstlink
+            if len(unvisited) != 0:
+                firstlink = unvisited.pop()
+                print "no valid links were found on", currenturl, "retrieving new link..."
+                currenturl = firstlink
+	    else:
+                print "there are no more links in the unvisited list"
+                break
 
 	#otherwise, fetch first link retrieved during current iteration
 	else:
             firstlink = temp.pop(0)
+
+            #pop link from unvisited
+            unvisited.remove(firstlink)
 		
 	    #free memory in temp list
 	    del temp[:]
 			
-	    #each time temp list is filled with 10 new links, a new depth has been reached
+	    #each time temp list is filled with n new links, a new depth has been reached
 	    depth = depth + 1
 	    print "current depth is", depth
 	    print "\n"
 
-	    #set current url to link fetched
+	    #set current url to link just fetched
 	    currenturl = firstlink
 		
-    #if error occurs while trying to open link, throw exception and pop a new link if there is one
     except (ValueError, urllib2.URLError, BadStatusLine, urllib2.HTTPError):
+        #if error occurs while trying to open link, throw exception and pop a new link if there is one
         if unvisited != []:
-			currenturl = unvisited.pop()
+	    currenturl = unvisited.pop()
 	#otherwise, alert the user that their link is invalid and there are no more links
 	else:
-	    print "Oops!", currenturl, "is not a valid url and there are no more links to parse"
+	    print "Unfortunately there are no more links to parse"
 	    break
